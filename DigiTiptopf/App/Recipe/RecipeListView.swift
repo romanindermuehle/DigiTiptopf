1972d86d78bad5d2974c95971481e2e13be5c004
Roman Indermühle <roman.indermuehle@icloud.com>
2024-11-17 21:35:45 +0100

    feat: sort recipeList by name, add ContentUnavailableView

--- DigiTiptopf/App/Recipe/RecipeListView.swift
+++ DigiTiptopf/App/Recipe/RecipeListView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct RecipeListView: View {
-    @Query var recipes: [Recipe]
+    @Query(sort: \Recipe.name) var recipes: [Recipe]
     @Query var users: [User]
     @Environment(\.modelContext) var context
     @Namespace var animation
@@ -18,33 +18,53 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView(.vertical, showsIndicators: false) {
-                LazyVGrid(columns: columns, spacing: 18) {
-                    ForEach(recipes) { recipe in
+            VStack {
+                if recipes.isEmpty {
+                    ContentUnavailableView {
+                        Label("Empty recipes", systemImage: "tray")
+                    } description: {
+                        Text("Create a new recipe to get started.")
+                    } actions: {
                         NavigationLink {
-                            RecipeDetailView(recipe: recipe, listStyle: users.first?.listStyle ?? "list")
-                                .navigationTransition(.zoom(sourceID: recipe.id, in: animation))
+                            RecipeModifyView(recipe: .constant(nil), listStyle: users.first?.listStyle ?? "list", name: "", ingredients: [], preparation: [], isEditing: false)
                         } label: {
-                            RecipeCard(imageData: recipe.image, recipeName: recipe.name)
-                                .matchedTransitionSource(id: recipe.id, in: animation)
+                            Label("Create recipe", systemImage: "plus")
                         }
-                        .contextMenu {
-                            Button(role: .destructive) {
-                                context.delete(recipe)
-                            } label: {
-                                Label("Delete Recipe", systemImage: "trash")
+                        .buttonStyle(.borderedProminent)
+                        .foregroundStyle(.black)
+                        
+                    }
+
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: columns, spacing: 18) {
+                            ForEach(recipes) { recipe in
+                                NavigationLink {
+                                    RecipeDetailView(recipe: recipe, listStyle: users.first?.listStyle ?? "list")
+                                        .navigationTransition(.zoom(sourceID: recipe.id, in: animation))
+                                } label: {
+                                    RecipeCard(imageData: recipe.image, recipeName: recipe.name)
+                                        .matchedTransitionSource(id: recipe.id, in: animation)
+                                }
+                                .contextMenu {
+                                    Button(role: .destructive) {
+                                        context.delete(recipe)
+                                    } label: {
+                                        Label("Delete Recipe", systemImage: "trash")
+                                    }
+                                    
+                                    NavigationLink {
+                                        RecipeModifyView(recipe: .constant(recipe), listStyle: users.first?.listStyle ?? "list", name: recipe.name, ingredients: recipe.ingredients, preparation: recipe.preparation, imageData: recipe.image, isEditing: true)
+                                    } label: {
+                                        Label("Edit Recipe", systemImage: "pencil")
+                                    }
+                                }
                             }
                             
-                            NavigationLink {
-                                RecipeModifyView(recipe: .constant(recipe), listStyle: users.first?.listStyle ?? "list", name: recipe.name, ingredients: recipe.ingredients, preparation: recipe.preparation, imageData: recipe.image, isEditing: true)
-                            } label: {
-                                Label("Edit Recipe", systemImage: "pencil")
-                            }
                         }
+                        .padding()
                     }
-                    
                 }
-                .padding()
             }
             .navigationTitle("Recipes")
             .toolbar {
