a07e676efda7748451ad8b017115bb884b7ca639
Roman Indermühle <roman.indermuehle@icloud.com>
2024-11-17 19:51:38 +0100

    refactor: make variable name plural

--- DigiTiptopf/Common/SampleData/SampleRecipeData.swift
+++ DigiTiptopf/Common/SampleData/SampleRecipeData.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 import SwiftData
 
-@MainActor let sampleRecipe: [Recipe] = [
+@MainActor let sampleRecipes: [Recipe] = [
     Recipe(name: "Pizza", ingredients: [Ingredient(name: "Tomato"), Ingredient(name: "Cheese")], preparation: [Step(name: "Mix all ingredients"), Step(name: "Bake in oven")], image: UIImage(resource: .pizza).jpegData(compressionQuality: 1)),
     Recipe(name: "Donut", ingredients: [Ingredient(name: "Flour"), Ingredient(name: "Sugar"), Ingredient(name: "Eggs")], preparation: [Step(name: "Mix all ingredients"), Step(name: "Bake in oven")], image: UIImage(resource: .donuts).jpegData(compressionQuality: 1)),
     Recipe(name: "Cake", ingredients: [Ingredient(name: "Flour"), Ingredient(name: "Sugar"), Ingredient(name: "Eggs")], preparation: [Step(name: "Mix all ingredients"), Step(name: "Bake in oven")])
@@ -19,7 +19,7 @@ struct SampleRecipeData: PreviewModifier {
     static func makeSharedContext() throws -> ModelContainer {
         let container = try ModelContainer(for: Recipe.self, User.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
         
-        for recipe in sampleRecipe {
+        for recipe in sampleRecipes {
             container.mainContext.insert(recipe)
         }
         
