1f49efdd084e53464d69c6947d5a058e4c45826d
Roman Indermühle <roman.indermuehle@icloud.com>
2024-11-18 15:48:37 +0100

    test: add UI and unit tests

--- DigiTiptopfTests/DigiTiptopfTests.swift
+++ DigiTiptopfTests/DigiTiptopfTests.swift
@@ -5,12 +5,22 @@
 //  Created by Roman Indermühle on 08.11.2024.
 //
 
+@testable import DigiTiptopf
 import Testing
+import Foundation
 
 struct DigiTiptopfTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    @MainActor
+    @Test func createFeedbackURLSuccessful() throws {
+        let settingsView = SettingsView()
+        
+        let feedbackURL = settingsView.createMailtoFeedbackURL()
+        
+        let expectedFeedbackURL: String = "mailto:rin140613@stud.gibb.ch?subject=DigiTiptopf%20-%20Feedback&body=%0A%0A-----%0ADigiTiptopf-Version:%201.0%20(1)%0AiOS-Version:%2018.1"
+        
+        guard let resultFeedbackURL = URL(string: expectedFeedbackURL) else { return }
+        
+        #expect(feedbackURL == resultFeedbackURL)
     }
-
 }
--- DigiTiptopfUITests/DigiTiptopfUITests.swift
+++ DigiTiptopfUITests/DigiTiptopfUITests.swift
@@ -8,36 +8,86 @@
 import XCTest
 
 final class DigiTiptopfUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
+    
     @MainActor
-    func testExample() throws {
+    func testCreateRecipe() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        
+        app.buttons["plus"].tap()
+        
+        let recipeNameTextField = app.textFields["recipeName"]
+        
+        recipeNameTextField.tap()
+        
+        recipeNameTextField.typeText("Donut")
+        
+        app.buttons["Add Ingredient"].tap()
+        
+        app.typeText("Flour")
+        
+        app.buttons["Add Ingredient"].tap()
+        
+        app.typeText("Sugar")
+        
+        app.buttons["Add Step"].tap()
+        
+        app.typeText("Mix")
+        
+        app.buttons["Add Step"].tap()
+        
+        app.typeText("Bake")
+        
+        app.buttons["Save"].tap()
+        
+        
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let donut = app.staticTexts["Donut"]
+        
+        XCTAssert(donut.exists)
     }
-
+    
     @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testExportRecipePDF() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        try testCreateRecipe()
+        
+        app.staticTexts["Donut"].tap()
+        
+        let menuButton = app
+            .buttons["menuButton"]
+            .buttons
+            .firstMatch
+            .descendants(matching: .any)
+            .firstMatch
+        
+        menuButton.tap()
+        
+        let sharePDFButton = app.buttons["sharePDFButton"]
+        
+        sharePDFButton.tap()
+        
+        let saveToFilesButton = app.descendants(matching: .any).matching(identifier: "Save to Files").firstMatch
+        
+        saveToFilesButton.tap()
+        
+        app.buttons["Save"].tap()
     }
+    
 }
